bfcc2932e21b3866cc07a0e65b1d91a00d28bb39
juniorba94 <juniorba94@outlook.com>
2019-07-23 11:34:38 -0300

    Bishop

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -51,17 +51,7 @@ public class Rook extends ChessPiece {
             mat[p.getRow()][p.getColumn()] = true;
         }
 
-        // left
-        p.setValues(position.getRow(), position.getColumn() - 1);
-        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
-            mat[p.getRow()][p.getColumn()] = true;
-            p.setColumn(p.getColumn() - 1);
-        }
-        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
-            mat[p.getRow()][p.getColumn()] = true;
-        }
-
-        // above
+        // down
         p.setValues(position.getRow() + 1, position.getColumn());
         while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
             mat[p.getRow()][p.getColumn()] = true;
